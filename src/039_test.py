32e34b130e0b40ad9aae14f0188603462e46c4d3
OkaNim <OKA.Hiroyuki@nims.go.jp>
2024-05-07 09:11:08 +0900

    Add files via upload

---
+++ src/039_test.py
@@ -0,0 +1,10 @@
+import src_039_recognize_polymer_name_240426 as src_039
+
+
+toks = ["in", "polystyrene-block-poly", '(', "ethylene", "oxide", ')', '(', "PS-b-PEO", ')', "bottlebrush", "block", "copolymers", '(', "BBCP", ')', "upon", "The", "BBCPs", "are", "soluble", "in", "organic", "solvents", "."]
+
+tags = src_039.main(toks)
+
+
+print("\ntags\n", tags)
+print("\n\n")
